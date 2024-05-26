fb73178ca888e97df5fdaf237a1de3fc48819014
Martin Faartoft <martin@ftft.dk>
2024-05-26 21:51:00 +0200

    add basic css styling and best recent score stat

--- MahjongDkStatsCalculators/StatsCalculators/BestScoreCalculator.cs
+++ MahjongDkStatsCalculators/StatsCalculators/BestScoreCalculator.cs
@@ -2,34 +2,55 @@
 
 internal class BestScoreCalculator : StatsCalculatorBase
 {
-    private Player _bestMcrScore = new Player(string.Empty, int.MinValue);
-    private Player _bestRiichiScore = new Player(string.Empty, int.MinValue);
-    
-    public override void AppendGame(Game game, GameType gameType)
-    {
-        var best = game.Players.MaxBy(p => p.Score)!;
+	private Player _bestMcrScore = new Player(string.Empty, int.MinValue);
+	private Player _bestRecentMcrScore = new Player(string.Empty, int.MinValue);
+	private Player _bestRiichiScore = new Player(string.Empty, int.MinValue);
+	private Player _bestRecentRiichiScore = new Player(string.Empty, int.MinValue);
 
-        if (gameType == GameType.Mcr)
-        {
-            _bestMcrScore = SelectBest(_bestMcrScore, best);
-        }
-        else if (gameType == GameType.Riichi)
-        {
-            _bestRiichiScore = SelectBest(_bestRiichiScore, best);
-        }
-    }
+	private DateOnly RecentIfGameAfter = DateOnly.FromDateTime(DateTime.UtcNow.AddYears(-1));
 
-    public override IEnumerable<Statistic> GetGlobalMcrStatistics()
-    {
-        return [ new Statistic("Best Score", $"{_bestMcrScore.Score} ({_bestMcrScore.Name})") ];
-    }
+	public override void AppendGame(Game game, GameType gameType)
+	{
+		var best = game.Players.MaxBy(p => p.Score)!;
 
-    public override IEnumerable<Statistic> GetGlobalRiichiStatistics()
-    {
-        return [new Statistic("Best Score", $"{_bestRiichiScore.Score} ({_bestRiichiScore.Name})")];
-    }
+		if (gameType == GameType.Mcr)
+		{
+			_bestMcrScore = SelectBest(_bestMcrScore, best);
 
-    private Player SelectBest(Player current, Player candidate) {
-        return candidate.Score > current.Score ? candidate : current;
-    }
+			if (game.DateOfGame > RecentIfGameAfter)
+			{
+				_bestRecentMcrScore = SelectBest(_bestRecentMcrScore, best);
+			}
+		}
+		else if (gameType == GameType.Riichi)
+		{
+			_bestRiichiScore = SelectBest(_bestRiichiScore, best);
+
+			if (game.DateOfGame > RecentIfGameAfter)
+			{
+				_bestRecentRiichiScore = SelectBest(_bestRecentRiichiScore, best);
+			}
+		}
+	}
+
+	public override IEnumerable<Statistic> GetGlobalMcrStatistics()
+	{
+		return [
+			new Statistic("Best Score", $"{_bestMcrScore.Score} ({_bestMcrScore.Name})"),
+			new Statistic("Best Score (last year)", $"{_bestRecentMcrScore.Score} ({_bestRecentMcrScore.Name})")
+		];
+	}
+
+	public override IEnumerable<Statistic> GetGlobalRiichiStatistics()
+	{
+		return [
+			new Statistic("Best Score", $"{_bestRiichiScore.Score} ({_bestRiichiScore.Name})"),
+			new Statistic("Best Score (last year)", $"{_bestRecentRiichiScore.Score} ({_bestRecentRiichiScore.Name})")
+		];
+	}
+
+	private Player SelectBest(Player current, Player candidate)
+	{
+		return candidate.Score > current.Score ? candidate : current;
+	}
 }
