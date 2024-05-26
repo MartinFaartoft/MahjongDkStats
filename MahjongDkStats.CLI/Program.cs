ff3902ae4f57e9a3065a7be004036350a147d589
Martin Faartoft <martin@ftft.dk>
2024-05-26 16:27:33 +0200

    working 'static site generator' structure using razor pages and AspNetCore HtmlRenderer

--- MahjongDkStats.CLI/Program.cs
+++ MahjongDkStats.CLI/Program.cs
@@ -1,5 +1,34 @@
 ﻿using MahjongDkStatsCalculators;
+using MahjongDkStats.CLI.Pages;
 using System.Text.Json;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+IServiceCollection services = new ServiceCollection();
+services.AddLogging();
+
+IServiceProvider serviceProvider = services.BuildServiceProvider();
+ILoggerFactory loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
+await using var htmlRenderer = new HtmlRenderer(serviceProvider, loggerFactory);
+
+var html = await htmlRenderer.Dispatcher.InvokeAsync(async () =>
+{
+    var dictionary = new Dictionary<string, object?>
+    {
+        { "Message", "Hello from the Render Message component!" }
+    };
+
+    var parameters = ParameterView.FromDictionary(dictionary);
+    var output = await htmlRenderer.RenderComponentAsync<IndexPage>(parameters);
+
+    return output.ToHtmlString();
+});
+
+Console.WriteLine(html);
+
 
 var url = "https://raw.githubusercontent.com/MartinFaartoft/MahjongDkScraper/main/data/mcr_games_full.json";
 var httpClient = new HttpClient();
@@ -12,4 +41,4 @@ foreach(var game in games)
     calc.AppendGame(game);
 }
 
-calc.Print();
+//calc.Print();
--- MahjongDkStatsCalculators/GameCountCalculator.cs
+++ MahjongDkStatsCalculators/GameCountCalculator.cs
@@ -1,34 +1,33 @@
-﻿namespace MahjongDkStatsCalculators
+﻿namespace MahjongDkStatsCalculators;
+
+public class GameCountCalculator : IStatsCalculator
 {
-    public class GameCountCalculator : IStatsCalculator
+    private Dictionary<string, int> GamesPlayed = [];
+
+    public void AppendGame(Game game)
     {
-        private Dictionary<string, int> GamesPlayed = [];
-
-        public void AppendGame(Game game)
+        foreach(var player in game.Players)
         {
-            foreach(var player in game.Players)
+            if (!GamesPlayed.ContainsKey(player.Name))
             {
-                if (!GamesPlayed.ContainsKey(player.Name))
-                {
-                    GamesPlayed.Add(player.Name, 0);
-                }
-                GamesPlayed[player.Name]++;
+                GamesPlayed.Add(player.Name, 0);
             }
+            GamesPlayed[player.Name]++;
         }
-
-        public void Print()
-        {
-
-            var sortedList = GamesPlayed.Select(kv => new GameCount(kv.Value, kv.Key)).OrderByDescending(gc => gc.Count);
-            Console.WriteLine("Most games played");
-            Console.WriteLine("-----------------");
-            foreach(var gc in sortedList)
-            {
-                Console.WriteLine(gc.Name + " - " + gc.Count);
-            }
-
-        }
-
-        public record GameCount(int Count, string Name);
     }
+
+    public void Print()
+    {
+
+        var sortedList = GamesPlayed.Select(kv => new GameCount(kv.Value, kv.Key)).OrderByDescending(gc => gc.Count);
+        Console.WriteLine("Most games played");
+        Console.WriteLine("-----------------");
+        foreach(var gc in sortedList)
+        {
+            Console.WriteLine(gc.Name + " - " + gc.Count);
+        }
+
+    }
+
+    public record GameCount(int Count, string Name);
 }
