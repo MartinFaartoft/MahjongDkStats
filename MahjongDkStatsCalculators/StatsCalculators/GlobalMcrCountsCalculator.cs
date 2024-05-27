b1f764d80d405bc1d1a0d375f0dfce236c40c3a1
Martin Faartoft <martin@ftft.dk>
2024-05-27 08:05:13 +0200

    fix using

--- MahjongDkStatsCalculators/StatsCalculators/GlobalMcrCountsCalculator.cs
+++ MahjongDkStatsCalculators/StatsCalculators/GlobalMcrCountsCalculator.cs
@@ -1,6 +1,4 @@
-﻿using System.Transactions;
-
-namespace MahjongDkStatsCalculators.StatsCalculators;
+﻿namespace MahjongDkStatsCalculators.StatsCalculators;
 
 internal class GlobalMcrCountsCalculator : GlobalCountsCalculator
 {
