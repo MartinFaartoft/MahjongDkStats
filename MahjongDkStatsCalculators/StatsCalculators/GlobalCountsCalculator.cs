ba740c1933fa7f8ad5287f3ad71ab9c8021a2123
Martin Faartoft <martin@ftft.dk>
2024-05-30 14:59:55 +0200

    add a few fun global stats

--- MahjongDkStatsCalculators/StatsCalculators/GlobalCountsCalculator.cs
+++ MahjongDkStatsCalculators/StatsCalculators/GlobalCountsCalculator.cs
@@ -13,12 +13,16 @@ internal class GlobalCountsCalculator : StatisticsCalculatorBase
         _handsCount += game.NumberOfWinds * game.Players.Count();
     }
 
+    private const decimal TileWidthInM = .02M;
+
     public override IEnumerable<Statistic> GetGlobalStatistics()
     {
         return [
             new Statistic("Games played", _gameCount.ToString()),
             new Statistic("Winds played", _windsCount.ToString()),
-			new Statistic("Hands played", _handsCount.ToString())
+			new Statistic("Hands played", _handsCount.ToString()),
+            new Statistic("Time spent shuffling and building walls", Math.Round(TimeSpan.FromMinutes(_handsCount * 2).TotalDays).ToString() + " days"),
+            new Statistic("Total length of walls built", Math.Round(_handsCount * 4 * 17 * TileWidthInM / 1000M).ToString() + " km")
             ];
     }
 }
