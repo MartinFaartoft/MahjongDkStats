45bfbd2d12bf88ef93825321f4058db27a6e825e
Martin Faartoft <martin@ftft.dk>
2024-06-11 11:52:31 +0200

    improve player page names

--- MahjongDkStats.CLI/NameSanitizer.cs
+++ MahjongDkStats.CLI/NameSanitizer.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace MahjongDkStats.CLI;
 
@@ -6,7 +8,38 @@ public static class NameSanitizer
 {
 	public static string SanitizeForUrlUsage(string s)
 	{
+		var cleaned = RemoveDiacritics(ReplaceNordicChars(s.ToLower()));
+
 		string pattern = @"[^A-Za-z0-9_\-]";
-		return Regex.Replace(s, pattern, string.Empty);
+		return Regex.Replace(cleaned, pattern, "-");
+	}
+
+
+	private static string ReplaceNordicChars(string s)
+	{
+		return s
+			.Replace("æ", "ae", StringComparison.OrdinalIgnoreCase)
+			.Replace("ø", "oe", StringComparison.OrdinalIgnoreCase)
+			.Replace("å", "aa", StringComparison.OrdinalIgnoreCase);
+	}
+
+	private static string RemoveDiacritics(string text)
+	{
+		var normalizedString = text.Normalize(NormalizationForm.FormD);
+		var stringBuilder = new StringBuilder(capacity: normalizedString.Length);
+
+		for (int i = 0; i < normalizedString.Length; i++)
+		{
+			char c = normalizedString[i];
+			var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+			if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+			{
+				stringBuilder.Append(c);
+			}
+		}
+
+		return stringBuilder
+			.ToString()
+			.Normalize(NormalizationForm.FormC);
 	}
 }
