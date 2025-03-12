51bb01cb38daea6cf35317dd92fd7cfe9f3a415c
Martin Faartoft <martin@ftft.dk>
2025-03-12 21:08:00 +0100

    add rating delta to latest games

--- MahjongDkStatsCalculators/Player.cs
+++ MahjongDkStatsCalculators/Player.cs
@@ -1,3 +1,8 @@
 ﻿namespace MahjongDkStatsCalculators;
 
 public record Player(string Name, int Score, decimal OldRating, decimal NewRating);
+
+public static class PlayerExtensions
+{
+	public static decimal RatingDelta(this Player p) => p.NewRating - p.OldRating;
+}
